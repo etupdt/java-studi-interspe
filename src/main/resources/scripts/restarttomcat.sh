edcab0465c8a2766d2f60ba209d449422d19e47a
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-05-13 17:26:35 +0200

    remove scripts obsolete

--- src/main/resources/scripts/restarttomcat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-log=/var/log/deploy/restarttomcat
-
-pwd > $log.loc
-chmod 755 /tmp/restarttomcat-aws.sh >> $log.loc
-
-/tmp/restarttomcat-aws.sh |& tee -a $log.log
--- src/main/resources/scripts/starttomcat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-log=/var/log/deploy/starttomcat
-
-pwd > $log.loc
-chmod 755 /tmp/starttomcat-aws.sh >> $log.loc
-
-/tmp/starttomcat-aws.sh |& tee $log.log
