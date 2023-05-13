0d5858b4071d74a909233169a0d1d049c1fe225d
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-05-13 16:36:29 +0200

    change tomcat port

--- scripts/restarttomcat.sh
+++ scripts/restarttomcat.sh
@@ -12,4 +12,4 @@ sudo docker rm tomcat-yourbook |& tee -a $log
 sudo docker image rm 498746666064.dkr.ecr.eu-west-3.amazonaws.com/tomcat-yourbook:latest
 
 sudo docker pull 498746666064.dkr.ecr.eu-west-3.amazonaws.com/tomcat-yourbook:latest |& tee -a $log
-sudo docker run -p 443:443 -d --name tomcat-yourbook 498746666064.dkr.ecr.eu-west-3.amazonaws.com/tomcat-yourbook |& tee -a $log
+sudo docker run -p 8443:8443 -d --name tomcat-yourbook 498746666064.dkr.ecr.eu-west-3.amazonaws.com/tomcat-yourbook |& tee -a $log
